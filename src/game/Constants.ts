b5fc7ec3064d9bef42949a87ba017b026030c534
Bill Baker <bill.baker@spensatech.com>
2018-01-15 20:40:17 -0500

    smaller default board

--- src/game/Constants.ts
+++ src/game/Constants.ts
@@ -8,8 +8,8 @@ export type MOVE_PLAYER = typeof MOVE_PLAYER;
 export const PLACE_CURSOR = 'PLACE_CURSOR';
 export type PLACE_CURSOR = typeof PLACE_CURSOR;
 
-export const INITIAL_WIDTH = 70;
-export const INITIAL_HEIGHT = 50;
+export const INITIAL_WIDTH = 36; // 70;
+export const INITIAL_HEIGHT = 27; // 50;
 export const INITIAL_POP = 300;
 
 export const INITIAL_STATE: StoreState = {
