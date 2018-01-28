7a469a4f9b0bba4594cbfadfbab8a76a8789755b
Bill Baker <bill.baker@spensatech.com>
2018-01-28 00:31:26 -0500

    defer perf test

--- src/game/PerfTest.tsx
+++ src/game/PerfTest.tsx
@@ -14,14 +14,21 @@ export interface PerfState {
 export function PerfReducer(
     state: PerfState = INITIAL_PERF_STATE, action: PerfAction
 ): PerfState {
+    // if (isQueuePerfTest(action)) {
+    //     console.log(`Queue perf test ${action.wh}`)
+    // }
     if (isRunPerfTest(action))
-        return { ...state, history: state.history.push(runPerfTest(state.inputs)) };
+        return { ...state, history: state.history.push(runPerfTest(action.wh)) };
     else if (isChangeWh(action))
         return { ...state, inputs: action.wh };
     else
         return state;
 }
 
+const QUEUE_PERF_TEST = 'QUEUE_PERF_TEST';
+type QUEUE_PERF_TEST = typeof QUEUE_PERF_TEST;
+// function isQueuePerfTest(action: PerfAction): action is QueuePerfTest {
+//     return action.type === QUEUE_PERF_TEST; }
 const RUN_PERF_TEST = 'RUN_PERF_TEST';
 type RUN_PERF_TEST = typeof RUN_PERF_TEST;
 function isRunPerfTest(action: PerfAction): action is RunPerfTest {
@@ -31,12 +38,14 @@ type CHANGE_WH = typeof CHANGE_WH;
 function isChangeWh(action: PerfAction): action is ChangeWh {
     return action.type === CHANGE_WH; }
 
-interface RunPerfTest extends GenericAction { type: RUN_PERF_TEST; }
+interface RunPerfTest extends GenericAction { type: RUN_PERF_TEST; wh: Wh; }
 interface ChangeWh extends GenericAction { type: CHANGE_WH; wh: Wh; }
-type PerfAction = RunPerfTest | ChangeWh;
+interface QueuePerfTest extends GenericAction { type: QUEUE_PERF_TEST; wh: Wh; }
+type PerfAction = RunPerfTest | ChangeWh | QueuePerfTest;
 
-function runPerfTestAction(): RunPerfTest { return {type: RUN_PERF_TEST}; }
 function changeWhAction(wh: Wh): ChangeWh { return {type: CHANGE_WH, wh: wh}; }
+// function queuePerfTestAction(wh: Wh): QueuePerfTest { return {type: QUEUE_PERF_TEST, wh: wh}; }
+function runPerfTestAction(wh: Wh): RunPerfTest { return {type: RUN_PERF_TEST, wh: wh}; }
 
 const mapStateToPerfProps = (state: PerfState) => ({
     history: state.history,
@@ -45,7 +54,10 @@ const mapStateToPerfProps = (state: PerfState) => ({
 
 const mapDispatchToPerfProps = (dispatch: Dispatch<PerfState>) => ({
     onChange: (wh: Wh) => { dispatch(changeWhAction(wh)); },
-    onRun: () => { dispatch(runPerfTestAction()); },
+    onQueue: (wh: Wh) => {
+        setTimeout((x: Wh) => dispatch(runPerfTestAction(x)), 0, wh);
+    },
+    onRun: (wh: Wh) => { dispatch(runPerfTestAction(wh)); },
 });
 
 export const runPerfTest = (wh: Wh) => {
@@ -58,13 +70,14 @@ export const runPerfTest = (wh: Wh) => {
 
 export interface PerfProps extends PerfState {
     onChange: (wh: Wh) => void;
-    onRun: () => void;
+    onRun: (wh: Wh) => void;
+    onQueue: () => void;
 }
 
 const PerfTest = (props: PerfProps) => {
     return (
         <div>
-            <WhInput wh={props.inputs} onChange={props.onChange} onTest={props.onRun}/>
+            <WhInput wh={props.inputs} onChange={props.onChange} onTest={props.onQueue}/>
             {
                 props.history.map((record: PerfRecord) => (
                     <PerfResultView record={record} />
@@ -80,12 +93,18 @@ export const PerfContainer = connect(mapStateToPerfProps, mapDispatchToPerfProps
 
 const WhInput = (props: WhInputProps) => (
     <div>
-        <NumberInput value={props.wh.w} label='width' key='width' onChange={
-            w => props.onChange(new Wh(w, props.wh.h))
-        } />
-        <NumberInput value={props.wh.h} label='width' key='width' onChange={
-            h => props.onChange(new Wh(props.wh.w, h))
-        } />
+        <NumberInput
+            value={props.wh.w}
+            label="width"
+            key="width"
+            onChange={w => props.onChange(new Wh(w, props.wh.h))}
+        />
+        <NumberInput
+            value={props.wh.h}
+            label="width"
+            key="width"
+            onChange={h => props.onChange(new Wh(props.wh.w, h))}
+        />
         <button onClick={() => props.onTest(props.wh)}>Test Performance</button>
     </div>
 );
@@ -110,7 +129,7 @@ const INITIAL_PERF_STATE = {
 };
 
 interface PerfResultProps {
-    record: PerfRecord
+    record: PerfRecord;
 }
 
 const PerfResultView = (props: PerfResultProps) => (
@@ -134,10 +153,14 @@ interface NumberInputProps {
 }
 
 const NumberInput = (props: NumberInputProps) => (
-    <label>{props.label}: <input type="number" value={props.value} onChange={
-        (e: React.ChangeEvent<HTMLInputElement>) => {
-            const str = e.currentTarget.value;
-            if (str) props.onChange(parseInt(str));
-        }
-    }/></label>
+    <label>{props.label}:
+        <input
+            type="number"
+            value={props.value}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                const str = e.currentTarget.value;
+                if (str) props.onChange(parseInt(str, 10));
+            }}
+        />
+    </label>
 );
