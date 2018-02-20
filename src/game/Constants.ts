463dafb31f566a25cbfc6b64d0555413fcbf9124
Bill Baker <bill.baker@spensatech.com>
2018-02-19 22:33:02 -0500

    SASS

--- src/game/Constants.ts
+++ src/game/Constants.ts
@@ -8,8 +8,8 @@ export type MOVE_PLAYER = typeof MOVE_PLAYER;
 export const PLACE_CURSOR = 'PLACE_CURSOR';
 export type PLACE_CURSOR = typeof PLACE_CURSOR;
 
-export const INITIAL_WIDTH = 7;
-export const INITIAL_HEIGHT = 37;
+export const INITIAL_WIDTH = 5;
+export const INITIAL_HEIGHT = 27;
 export const INITIAL_POP = 120;
 
 export const INITIAL_STATE: BoardState = {
