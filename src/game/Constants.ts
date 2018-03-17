02355c4bcce7c6edbcde2636b2b40fe8ddc2bcce
Bill Baker <bill@orangecrayon.com>
2018-03-17 17:41:59 -0400

    temp fix for displaySize in state

--- src/game/Constants.ts
+++ src/game/Constants.ts
@@ -1,6 +1,7 @@
 import {Board, PLAYABLE_PLAYERS, RandomArranger} from './Board';
 import {BoardViewState} from './BoardContainer';
 import {HexCoord} from './Hex';
+import Dimension from "../Dimension";
 
 export const INITIAL_WIDTH = 5;
 export const INITIAL_HEIGHT = 27;
@@ -10,7 +11,8 @@ export const INITIAL_STATE: BoardViewState = {
     board: Board.constructRectangular(
         INITIAL_WIDTH,
         INITIAL_HEIGHT,
-        new RandomArranger(INITIAL_POP, PLAYABLE_PLAYERS)
+        new RandomArranger(INITIAL_POP, PLAYABLE_PLAYERS),
     ),
     cursor: HexCoord.NONE,
+    displaySize: new Dimension(1000, 600),
 };
