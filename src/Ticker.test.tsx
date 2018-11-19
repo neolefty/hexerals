32a96030fbed908a6741af4beb0d7184e9df37a3
Bill Baker <bill@orangecrayon.com>
2018-11-18 21:50:43 -0500

    ticker

---
+++ src/Ticker.test.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import * as enzyme from 'enzyme';
+import {Ticker} from './Ticker';
+import * as Adapter from 'enzyme-adapter-react-16';
+
+jest.useRealTimers()
+
+const sleep = (ms: number) => {
+    return new Promise(
+        resolve => setTimeout(
+            () => resolve(`${ms} ms elapsed`),
+            ms,
+        )
+    )
+}
+
+const createTicker = (tick: () => void): JSX.Element => (
+    <Ticker
+        tick={tick}
+        tickMillis={10}
+    >
+        <p>1</p>
+        <p>2</p>
+    </Ticker>
+)
+
+it('renders children', () => {
+    enzyme.configure({adapter: new Adapter()});
+    const ticker = createTicker(() => {})
+    expect(enzyme.render(ticker).text()).toBe('12')
+})
+
+it('creates a timer', async () => {
+    let n = 0;
+    const ticker = createTicker(() => {
+        n += 1
+    })
+    const wrapper = enzyme.mount(ticker)
+    await sleep(25)
+    expect(n).toBeGreaterThanOrEqual(2)
+    wrapper.unmount()
+    const m = n
+    await sleep(25)
+    expect(m).toBe(n)
+    expect(n).toBeLessThan(4)
+
+    // sleep(25).then(() => {
+    //     expect(n).toBeGreaterThanOrEqual(2)
+    //     const m = n
+    //     wrapper.unmount()
+    // })
+});
---
+++ src/Ticker.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import {Component} from 'react';
+
+// as long as this is present, it will tick
+export interface TickerProps {
+    tickMillis: number,
+    tick: () => void,
+    children?: JSX.Element | JSX.Element[];
+}
+
+export class Ticker extends Component<TickerProps> {
+    private timer?: NodeJS.Timer;
+
+    componentWillMount(): void {
+        if (!this.timer)
+            this.timer = global.setInterval(this.props.tick, this.props.tickMillis);
+    }
+
+    componentWillUnmount(): void {
+        if (this.timer) {
+            global.clearInterval(this.timer);
+            this.timer = undefined;
+        }
+    }
+
+    render(): React.ReactNode {
+        return this.props.children
+            ? this.props.children
+            : <div/>
+    }
+}
