004236cd19840feadd2b1193389e0e043cc44e17
Bill Baker <bbbaker@alum.mit.edu>
2018-11-25 17:10:11 +0000

    copy to deploy dir

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Put something like this in your crontab:
+# /5 * * * * "cd ~/source/hexerals && ./deploy.sh"
+# 0,5,10,15,20,25,30,35,40,45,50,55 * * * * "cd ~/source/hexerals && ./deploy.sh"
+
 git pull
 GIT_PULL_EXIT=$?
 if [[ $GIT_PULL_EXIT != 0 ]]; then
@@ -9,7 +13,7 @@ fi
 
 LATEST_HASH=`git log -n 1 --pretty=format:"%H"`
 
-if [[ -d "build" && -f "last-build-hash" ]]; then
+if [[ -d "build" && -f "last-build-hash" && -f "deploy_dir/index.html" ]]; then
 #    echo "---- latest hash = ${last-build-hash}"
     echo $LATEST_HASH | diff -q -w "last-build-hash" -
     DIFF=$?
@@ -33,7 +37,11 @@ if [ $SHOULD_BUILD == true ]; then
 	yarn build
 	BUILD_EXIT=$?
 	if [[ $BUILD_EXIT == 0 ]]; then
-	    echo $LATEST_HASH > "last-build-hash"
+	    cp -r build/* deploy_dir
+	    CP_EXIT=$?
+	    if [[ $CP_EXIT == 0 ]]; then
+		echo $LATEST_HASH > "last-build-hash"
+	    fi
 	fi
     fi
 else
