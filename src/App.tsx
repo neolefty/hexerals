b8285e08dc0a13b269ffd4bf8f75357db143bbc1
Bill Baker <bill@orangecrayon.com>
2018-11-26 22:55:59 -0500

    put local game and color wheel into tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,71 +1,79 @@
-import * as React from 'react';
-import './App.css';
-import {MIN_HEIGHT} from './game/board/BoardConstants';
-import Dimension from './Dimension';
-import {ColorsContainer, ColorsState} from './color/ColorsContainer';
-import {CycleState} from './game/cycle/CycleState';
-import {CycleContainer} from './game/cycle/CycleContainer';
-import {MIN_WIDTH} from './game/board/BoardConstants';
+import * as React from 'react'
+import './App.css'
+import {MIN_HEIGHT} from './game/board/BoardConstants'
+import Dimension from './Dimension'
+import {ColorsContainer, ColorsState} from './color/ColorsContainer'
+import {CycleState} from './game/cycle/CycleState'
+import {CycleContainer} from './game/cycle/CycleContainer'
+import {MIN_WIDTH} from './game/board/BoardConstants'
+import {Tab, TabList, TabPanel, Tabs} from 'react-tabs'
 
 export interface GenericAction {
-    type: string;
+    type: string
 }
 
 export interface AppProps {}
 
 export interface AppState {
-    colors: ColorsState;
-    cycle: CycleState;
+    colors: ColorsState
+    cycle: CycleState
 
     // displaySize is only needed here so that setState in updateDimensions()
     // will trigger rendering of children
-    displaySize: Dimension;
+    displaySize: Dimension
 }
 
 export interface AppProps {
 }
 
 class App extends React.Component<AppProps, AppState> {
-    private dimensionListener = this.updateDimensions.bind(this);
+    private dimensionListener = this.updateDimensions.bind(this)
 
     updateDimensions() {
         const dim = new Dimension(
             Math.max(window.innerWidth, MIN_WIDTH),
             Math.max(window.innerHeight - 25, MIN_HEIGHT)
-        );
+        )
         this.setState({
             ...this.state,
             displaySize: dim,
-        });
+        })
     }
 
     componentDidMount(): void {
-        window.addEventListener('resize', this.dimensionListener);
-        this.updateDimensions();
+        window.addEventListener('resize', this.dimensionListener)
+        this.updateDimensions()
     }
 
     componentWillUnmount(): void {
-        window.removeEventListener('resize', this.dimensionListener);
+        window.removeEventListener('resize', this.dimensionListener)
     }
 
     render() {
+        const displaySize = this.getDisplaySize().plus(0,-30)
         return (
             <div className="App">
-                <CycleContainer
-                    displaySize={this.getDisplaySize()}
-                />
-                <ColorsContainer
-                    displaySize={this.getDisplaySize().scale(0.5)}
-                />
+                <Tabs>
+                    <TabList>
+                        <Tab>Local Game</Tab>
+                        <Tab>Color Wheel</Tab>
+                    </TabList>
+                    <TabPanel>
+                        <CycleContainer displaySize={displaySize}/>
+                    </TabPanel>
+                    <TabPanel>
+                        <ColorsContainer displaySize={displaySize.scale(0.5)}/>
+                    </TabPanel>
+                </Tabs>
             </div>
-        );
+        )
     }
 
     private getDisplaySize() {
         return (this.state && this.state.displaySize)
             ? this.state.displaySize
-            : new Dimension(MIN_WIDTH, MIN_HEIGHT);
+            : new Dimension(MIN_WIDTH, MIN_HEIGHT)
     }
 }
 
-export default App;
+export default App
