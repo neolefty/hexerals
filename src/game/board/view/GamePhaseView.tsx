a106a9f83e6d21476b95aef56b1695e07ba0ac25
Bill Baker <bill@orangecrayon.com>
2019-02-09 12:54:50 -0500

    victory and defeat, at last

--- src/game/board/view/GamePhaseView.tsx
+++ src/game/board/view/GamePhaseView.tsx
@@ -1,14 +1,81 @@
 import * as React from 'react'
 import {BoardViewProps} from './BoardViewBase'
 import './GamePhaseView.css'
+import {Tile} from '../model/Tile'
+import {Hex} from '../model/Hex'
+import {List, Map} from 'immutable'
+import {Player} from '../model/players/Players'
+import {Terrain} from '../model/Terrain'
+import {HexBoardView} from './HexBoardView';
+import {CartPair} from '../../../common/CartPair';
+import {Board} from '../model/Board';
+import {MovementQueue} from '../model/MovementQueue';
 
-export const Ended = (props: BoardViewProps) => (
+const singleHexBoard = (
+    props: BoardViewProps, tile: Tile,
+    grabFocus: boolean = false, fraction: number = 3,
+): BoardViewProps => ({
+    ...props,
+    grabFocus: grabFocus,
+    displaySize: new CartPair(
+        props.displaySize.min / fraction,
+        props.displaySize.min / fraction
+    ),
+    boardState: {
+        ...props.boardState,
+        board: Board.constructSquare(
+            1, List<Player>(props.boardState.curPlayer || Player.Zero)
+        ).setTiles(Map<Hex, Tile>([[Hex.ORIGIN, tile]])),
+        moves: new MovementQueue(),
+    }
+})
+
+export const Victory = (props: BoardViewProps) => (
     <div className='GamePhaseView'>
         <div className='Modal'>
-            It's Over!
-            <button onMouseDown={props.onEndGame}>
-                Done
-            </button>
+            <div className='Column'>
+                <div className='Row'>
+                    <HexBoardView {
+                        ...singleHexBoard(
+                            props,
+                            new Tile(Player.Zero, 0, Terrain.Capital)
+                        )
+                    }/>
+                </div>
+                <div className='Row'>
+                    <strong>Victory!</strong>
+                </div>
+                <div className='Row'>
+                    <button onClick={props.onEndGame}>
+                        Back
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+export const Defeat = (props: BoardViewProps) => (
+    <div className='GamePhaseView'>
+        <div className='Modal'>
+            <div className='Column'>
+                <div className='Row'>
+                    <HexBoardView {
+                        ...singleHexBoard(
+                            props,
+                            new Tile(Player.Zero, 0, Terrain.Mountain)
+                        )
+                    }/>
+                </div>
+                <div className='Row'>
+                    <strong>Defeat.</strong>
+                </div>
+                <div className='Row'>
+                    <button onClick={props.onEndGame}>
+                        Back
+                    </button>
+                </div>
+            </div>
         </div>
     </div>
 )
--- src/game/board/view/LocalGameView.tsx
+++ src/game/board/view/LocalGameView.tsx
@@ -2,7 +2,13 @@ import * as React from 'react'
 import {TickerBoardView, TickerBoardViewProps} from './TickerBoardView'
 import {Layered} from '../../../common/Layered'
 import {GamePhase} from '../model/GamePhase'
-import {Ended} from './GamePhaseView'
+import {Defeat, Victory} from './GamePhaseView'
+
+const curPlayerTileCount = (props: TickerBoardViewProps) => (
+    props.boardState.curPlayer !== undefined
+    && props.boardState.board.getTileStatistics()
+        .get(props.boardState.curPlayer, 0)
+) || 0
 
 export const LocalGameView = (
     props: TickerBoardViewProps
@@ -11,7 +17,13 @@ export const LocalGameView = (
         <TickerBoardView {...props} />
         {
             props.boardState.phase === GamePhase.Ended
-                ? <Ended{...props}/>
+            && curPlayerTileCount(props) > 0
+                ? <Victory{...props}/>
+                : undefined
+        }
+        {
+            curPlayerTileCount(props) === 0
+                ? <Defeat{...props}/>
                 : undefined
         }
     </Layered>
