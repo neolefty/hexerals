81a45eea8e394ba5f871184049389d980e47fa0f
Beecher Baker <bbbaker@alum.mit.edu>
2023-10-23 20:32:07 -0400

    migrate to nx 17 and update immutable

--- v2/libs/hexlib/src/lib/Environment.spec.ts
+++
@@ -1,7 +0,0 @@
-import { inTest } from "./Environment"
-
-describe("Environment", () => {
-    it("should be in test mode", () => {
-        expect(inTest()).toBeTruthy()
-    })
-})
--- v2/libs/hexlib/src/lib/Environment.ts
+++
@@ -1,9 +0,0 @@
-export const inDev = () => process.env?.["NODE_ENV"] === "development"
-export const inProd = () => process.env?.["NODE_ENV"] === "production"
-export const inTest = () => process.env?.["NODE_ENV"] === "test"
-
-export const devAssert = (condition: unknown, message?: string) => {
-    if (!inProd() && !condition) throw new Error(message)
-}
-
-export const APP_NAME = "Hexerals"
