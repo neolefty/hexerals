1c8a750e7356c64600c808d50b7e9c1e659c66b9
Beecher Baker <bbbaker@alum.mit.edu>
2024-06-30 17:19:42 -0400

    split board state from positions; add logic for finding neighbors and visible spots

--- v3/packages/game-model/src/Board.ts
+++ v3/packages/game-model/src/Board.ts
@@ -1,11 +1,3 @@
-interface BoardTopology {
-    /**
-     * Each coord is identified by an integer.
-     * Each coord has a list of neighbors.
-     */
-    readonly neighbors: ReadonlyArray<ReadonlyArray<number>>
-}
-
 /**
  * Each coord is identified by an integer.
  * Each coord has an x-y location on the board, which is purely cosmetic.
@@ -17,6 +9,20 @@ interface Coord {
     x: number
     /** Cosmetic y-position of the coordinate. */
     y: number
+}
+
+interface BoardTopology {
+    /**
+     * Each coord is identified by an integer.
+     * Each coord has a list of neighbors.
+     * Note that the top level (list of coord IDs) is sparse; use for ... in to iterate.
+     */
+    readonly neighbors: ReadonlyArray<ReadonlyArray<number>>
+}
+
+interface BoardGeometry {
+    /** A board's spatial locations; topology is derived from it via a Voronoi diagram. */
+    readonly locations: ReadonlyArray<Coord>
 }
 
 type Terrain = "land" | "mountain" | "city" | "capital"
@@ -33,12 +39,13 @@ interface SpotContents {
 /** A position on a playing board. */
 interface Spot {
     /** The coord of the spot. */
-    coord: Coord
+    locationId: number
     contents: SpotContents
 }
 
 export interface Board {
     topology: BoardTopology
+    geometry: BoardGeometry
     spots: ReadonlyArray<Spot>
 }
 
@@ -51,10 +58,49 @@ export const createDumbBoard: () => Board = () => ({
             [0, 1, 2],
         ],
     },
+    geometry: {
+        locations: [
+            { id: 0, x: 0, y: 0 },
+            { id: 1, x: 1, y: 0 },
+            { id: 2, x: 0, y: 1 },
+            { id: 3, x: 1, y: 1 },
+        ],
+    },
     spots: [
-        { coord: { id: 0, x: 0, y: 0 }, contents: { owner: 0, pop: 1, terrain: "land" } },
-        { coord: { id: 1, x: 1, y: 0 }, contents: { owner: 0, pop: 1, terrain: "land" } },
-        { coord: { id: 2, x: 0, y: 1 }, contents: { owner: 0, pop: 1, terrain: "land" } },
-        { coord: { id: 3, x: 1, y: 1 }, contents: { owner: 0, pop: 1, terrain: "land" } },
+        { locationId: 0, contents: { owner: 0, pop: 1, terrain: "land" } },
+        { locationId: 1, contents: { owner: 0, pop: 50, terrain: "city" } },
+        { locationId: 2, contents: { owner: 0, pop: 1, terrain: "mountain" } },
+        { locationId: 3, contents: { owner: 1, pop: 4, terrain: "capital" } },
     ],
 })
+
+const emptyNumbers: ReadonlyArray<number> = []
+
+/** Neighbors of a spot, up to a distance of `depth`, in index order. */
+export const neighbors = (spotId: number, topology: BoardTopology, depth: number, visited?: Set<Number>): ReadonlyArray<number> => {
+    if (depth === 0) return emptyNumbers
+    if (depth === 1) return topology.neighbors[spotId] || emptyNumbers
+    else {
+        const result = []
+        const visitedLocal = visited || new Set<number>()
+        for (const neighbor of topology.neighbors[spotId] || emptyNumbers) {
+            if (!visitedLocal.has(neighbor)) {
+                visitedLocal.add(neighbor)
+                result.push(...neighbors(neighbor, topology, depth - 1, visitedLocal))
+            }
+        }
+        const unique = new Set(result)
+        unique.delete(spotId)
+        return [...unique.values()].sort()
+    }
+}
+
+/** What spots are visible to a player, given a view distance? */
+export const visibleSpots = (board: Board, player: number, viewDistance: number = 1): Board => {
+    const visibleSpots = board.spots.filter(spot => {
+        if (spot.contents.owner === player) return true
+        const result = neighbors( spot.locationId, board.topology, viewDistance)
+        return result.some(id => board.spots[id]!.contents.owner === player)
+    })
+    return { ...board, spots: visibleSpots }
+}
