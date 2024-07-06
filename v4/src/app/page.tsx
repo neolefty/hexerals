3fa9c560f02a9fef63d16ca1c7ff3bcc6877a0e8
Beecher Baker <bbbaker@alum.mit.edu>
2024-07-06 19:45:33 -0400

    Next template app, cleaned up a bit

---
+++ v4/src/app/page.tsx
@@ -0,0 +1,11 @@
+import styles from "./page.module.css"
+
+export default async function Home() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <h1>Hexerals v4</h1>
+      </main>
+    </div>
+  )
+}
