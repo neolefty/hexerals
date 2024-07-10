114d545fbae2106fab8cd8bff39721351b6c0a95
Beecher Baker <bbbaker@alum.mit.edu>
2024-07-10 17:32:43 -0400

    configure eslint for TS; experiment with use()

--- v4/src/app/page.tsx
+++ v4/src/app/page.tsx
@@ -3,12 +3,12 @@ import { createDumbBoard } from "./_lib/game-model/GenerateBoard"
 
 export default async function Home() {
     const board = createDumbBoard()
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <h1>Hexerals v4</h1>
-          <p>A simple board has {board.spots.length} spots.</p>
-      </main>
-    </div>
-  )
+    return (
+        <div className={styles.page}>
+            <main className={styles.main}>
+                <h1>Hexerals v4</h1>
+                <p>A simple board has {board.spots.length} spots.</p>
+            </main>
+        </div>
+    )
 }
